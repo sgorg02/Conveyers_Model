3766720fcdbaf850a764347105c2567378242558
sgorg <sgorg2002@gmail.com>
2023-04-06 11:00:13 +0300

    Create project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.conveyers_model {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.conveyers_model to javafx.fxml;
+    exports com.example.conveyers_model;
+}
